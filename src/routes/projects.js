4b10f81b993e8c662e96af378c838d86265f75fd
Masooma Jafari <masoomajafari@Mac.attlocal.net>
2025-04-15 21:56:28 -0700

    removed swagger and unneccessary comments  from this branch

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -18,70 +18,11 @@ const {
 } = require('../controllers/projects');
 
 const router = express.Router();
-
-/**
- * @swagger
- * tags:
- *   name: Projects
- *   description: Project management endpoints
- */
-
-/**
- * @swagger
- * /projects:
- *   get:
- *     summary: Get all projects
- *     tags: [Projects]
- *     responses:
- *       200:
- *         description: List of projects
- *   post:
- *     summary: Create a new project
- *     tags: [Projects]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/ProjectInput'
- *     responses:
- *       201:
- *         description: Project created successfully
- */
 router
   .route('/')
   .get(getAllProjects)
   .post(validate(projectValidationSchema), createProject);
 
-/**
- * @swagger
- * /projects/{id}:
- *   get:
- *     summary: Get project by ID
- *     tags: [Projects]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: Project ID
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Project details
- *   delete:
- *     summary: Delete a project
- *     tags: [Projects]
- *   patch:
- *     summary: Update a project
- *     tags: [Projects]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/ProjectInput'
- */
 router
   .route('/:id')
   .all(fetchProjectMiddleware)
@@ -89,57 +30,17 @@ router
   .delete(deleteProject)
   .patch(validate(projectValidationSchema), updateProject);
 
-/**
- * @swagger
- * /projects/{id}/leave:
- *   post:
- *     summary: Leave a project
- *     tags: [Projects]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Successfully left project
- */
-router.route('/:id/leave').post(fetchProjectMiddleware, leaveProject);
+router
+.route('/:id/leave')
+.post(fetchProjectMiddleware, leaveProject);
 
-/**
- * @swagger
- * /projects/{id}/join-requests:
- *   post:
- *     summary: Send a join request to a project
- *     tags: [Projects]
- *   get:
- *     summary: Get all join requests for a project
- *     tags: [Projects]
- *   delete:
- *     summary: Unsend a join request
- *     tags: [Projects]
- */
 router
   .route('/:id/join-requests')
   .all(fetchProjectMiddleware)
   .post(sendJoinRequest)
   .get(getProjectJoinRequests)
   .delete(unsendJoinRequest);
-
-/**
- * @swagger
- * /projects/{id}/votes:
- *   post:
- *     summary: Add a vote to a project
- *     tags: [Projects]
- *   get:
- *     summary: Get all votes for a project
- *     tags: [Projects]
- *   delete:
- *     summary: Remove your vote from a project
- *     tags: [Projects]
- */
+  
 router
   .route('/:id/votes')
   .all(fetchProjectMiddleware)
