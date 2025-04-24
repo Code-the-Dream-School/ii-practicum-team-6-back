5ad064bdfec9401122e480b1249e84e361ee97d6
Val Lyzhyn <1instavl@gmail.com>
2025-04-24 11:33:30 -0700

    fix project model import bug

--- src/middleware/fetchProjectMiddleware.js
+++ src/middleware/fetchProjectMiddleware.js
@@ -1,4 +1,4 @@
-const Project = require('../models/Project');
+const Project = require('../models/project');
 const { NotFoundError } = require('../errors');
 
 const fetchProject = async (req, res, next) => {
