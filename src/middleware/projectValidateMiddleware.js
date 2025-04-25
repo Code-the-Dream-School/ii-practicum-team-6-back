1137b0b530398329d9f8bd1d7eddd759c1beaeab
Masooma Jafari <masoomajafari@Mac.attlocal.net>
2025-04-25 11:57:41 -0700

    update swagger for votes and join request

--- src/middleware/projectValidateMiddleware.js
+++ src/middleware/projectValidateMiddleware.js
@@ -1,5 +1,7 @@
 module.exports = (schema) => {
     return (req, res, next) => {
+      console.log('schema',schema);
+      
       const { error, value } = schema.validate(req.body);
       if (error) {
         return res.status(400).json({ msg: error.details[0].message });
