dcb0e057da5c4d34ba3ac833d47310f969b34fa5
Val Lyzhyn <1instavl@gmail.com>
2025-04-28 15:46:46 -0700

    render fix

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,97 +3,14 @@ const authService = require('../services/authServices')
 const { toUserResponseDto } = require('../dtos/user.dto')
 
 
-exports.signUp = async (req, res, next) => {
+exports.getAllCommentsByTheProject = async (req, res, next) => {
     try {
-        const { username, email, password } = req.body;
-        const user = await authService.signUp(username, email, password);
-        res.status(201).json({
-            success : true,
-            message: 'User Created Successfully',
-            data: {user : toUserResponseDto(user)}
-        })
+       
     }
     catch (err) {
         next(err);
     }
 }
 
-
-exports.signIn = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        const { token, user } = await authService.signIn(email, password);
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'None'
-        });
-        res.status(200).json({
-            success : true,
-            message: 'Authentication Successfull',
-            data: {user : toUserResponseDto(user)}
-        });
-    }
-    catch (err) {
-        next(err);
-    }
-}
-
-exports.signOut = (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        if (!token) {
-            throw new BadRequestError('User already logged out');
-        }
-        res.clearCookie('token', { httpOnly: true, sameSite: 'None', secure: true });
-        res.status(200).json({
-            success : true,
-            message: 'Users Loged out Successfully',
-        });
-    }
-    catch (err) {
-        next(err);
-    }
-};
-
-exports.authMe = (req, res, next) => {
-    console.log("auth")
-    try {
-        const user = req.user;
-        res.status(200).json({
-            success : true,
-            message: 'Authentication Successfull',
-            data: {user : toUserResponseDto(user)}
-        });
-    }
-    catch (err) {
-        next(err);
-    }
-};
-
-exports.forgotPassword = async (req, res, next) => {
-
-    try {
-        const email = req.body.email;
-        await authService.forgotPassword(email);
-        res.status(200).send('Check your email for instructions on resetting your password');
-    }
-    catch (err) {
-        next(err);
-    }
-
-};
-
-exports.resetPassword = async (req, res, next) => {
-    try {
-        const { token } = req.params;
-        const { newPassword } = req.body;
-        await authService.resetPassword(token, newPassword);
-        res.status(200).send('Password updated successfully');
-    }
-    catch (err) {
-        next(err);
-    }
-}
 
 
