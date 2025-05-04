912d7e571e608e4ca8c4e0d8927169ce745cbba7
Val Lyzhyn <1instavl@gmail.com>
2025-05-03 20:14:01 -0700

    delete logs

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -12,29 +12,12 @@ exports.authenticate = async (req, res, next) => {
         }
     
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded.user.id)
         const user = await User.findById(decoded.user.id).populate('skills', 'name');
-        console.log(user)
         if (!user) return res.status(404).json({ message: 'User not found' });
     
         req.user = user;
         next();
-    // try{
-      
-        
-    //     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    //         if (err) {
-    //             return res.status(401).json({ message: 'Invalid token' });
-    //         }
-            
-    //         const currentTime = Math.floor(Date.now() / 1000); 
-    //         const user =  User.findById(decoded.id);
-    //         if (decoded.exp < currentTime) {
-    //             return res.status(401).json({ message: 'Token expired' });
-    //         }
-    //         req.user = user; 
-    //         next();
-    //     })
+   
     }
     
     catch (error) {
