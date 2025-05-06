e42860ed1c1be2ab3affbe7335a378301a179de2
Val Lyzhyn <1instavl@gmail.com>
2025-05-06 09:55:48 -0700

    jwt fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -26,7 +26,7 @@ exports.signIn = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'None'
+            sameSite: 'Lax'
         });
         res.status(200).json({
             success : true,
